684ec7ed52a0d5f33ff840208c86171c5eaae58a
Carl Furrow <me@carlfurrow.com>
2012-07-15 12:46:13 -0400

    Always ignore .ignore-warn elements. Changed function casing.
    - Added minified js file as well.

--- src/jquery.warnunload.js
+++ src/jquery.warnunload.js
@@ -1,21 +1,31 @@
 /*
-*	Class: warnUnload
-*	Use: Warn user if data is unsaved when they try to browse away from current page
-*	Author: Carl Furrow (http://cfurrow.tumblr.com)
-*	Version: 0.0.2 
+* Class: warnUnload
+* Use: Warn user if data is unsaved when they try to browse away from current page
+* Author: Carl Furrow (http://carlfurrow.com)
+* Version: 0.0.2
 */
+/*global opera:true */
 (function($){
   if(typeof $ === "undefined" || $ === null){
     return;
   }
+  function addIgnoreWarnToIgnore(options){
+    if(options.ignore === null || options.ignore === ""){
+      options.ignore = ".ignore-warn";
+    }
+    if(!/\.ignore-warn/.test(options.ignore)){
+      options.ignore += ", .ignore-warn";
+    }
+  }
   $.warnUnload = function(options){
     var DEFAULTS = {
-      message:"Are you sure you want to leave this page?"
-      ,urls:[]
-      ,ignore:""
-      ,after:function(){}
-      ,onItem:function(item){return true;}};
+      message:"Are you sure you want to leave this page?",
+      urls:[],
+      ignore:".ignore-warn",
+      after:function(){},
+      onItem:function(item){return true;}};
     options = $.extend(DEFAULTS,options);
+    addIgnoreWarnToIgnore(options);
     var $inputs = $("input:text,input:checkbox,input:radio,select");
 
     try{
@@ -23,6 +33,7 @@
       opera.setOverrideHistoryNavigationMode('compatible');
       history.navigationMode = 'compatible';
     }catch(e){}
+
 
     function setupChangeEventsOnInputs(){
       $inputs.change(markAsChanged);
@@ -48,28 +59,28 @@
 
       if(warn){
         options.after();
-        return ReturnMessage(); 
+        return returnMessage();
       }
       // don't return anything.
     }
 
-    function ReturnMessage()
+    function returnMessage()
     {
       return options.message;
     }
  
-    function UnBindWindow()
+    function unBindWindow()
     {
       $(window).unbind('beforeunload', DoWeShowConfirmMessage);
     }
 
-    function UnBindIgnores(){
-      $(options.ignore).bind('click', UnBindWindow); 
+    function unBindIgnores(){
+      $(options.ignore).bind('click', unBindWindow);
     }
 
     //Bind Exit Message Dialogue
-    setupChangeEventsOnInputs(); 
-    UnBindIgnores();
+    setupChangeEventsOnInputs();
+    unBindIgnores();
     
     $(window).bind('beforeunload', DoWeShowConfirmMessage);
   };
